e7ccaa8c96a2a9e4ec054de028d10ef2a5bf5947
simonsmadsen <simon@smadsen.com>
2017-09-18 16:39:03 +0200

    new

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const web = require('js-web')
+import {
+  htmlRoute, route, postRoute, twitterRoute, notFound,
+  twitterRequestUrl,
+  helpers,
+  request,
+  storage,
+  social,
+  redirect,
+  inject,
+  start,
+  back
+} from 'js-web'
 
-const db = web.storage.mysql
+const { local, mysql } = storage
+
 const auth = require('./src/auth.js')
 const parseInputText = require('./src/parse-input-text.js')
 
@@ -8,19 +20,19 @@ const parseInputText = require('./src/parse-input-text.js')
  * Injections
 */
 const injections = [
-  web.inject.googleAnalytics(),
-  web.inject.jquery(),
-  web.inject.bootstrap(),
-  web.inject.style('style/syntax.css'),
-  web.inject.style('style/style.sass'),
-  web.inject.script('script/main.js')
+  inject.googleAnalytics(),
+  inject.jquery(),
+  inject.bootstrap(),
+  inject.style('style/syntax.css'),
+  inject.style('style/style.sass'),
+  inject.script('script/main.js')
 ]
 const questionInjections = injections.concat([
-  web.inject.style('style/questions.css')
+  inject.style('style/questions.css')
 ])
 const authInjections = questionInjections.concat([
-  web.inject.googleAuth('/google-login', 'google-login'),
-  web.inject.facebookAuth('/facebook-login', 'fb-login')
+  inject.googleAuth('/google-login', 'google-login'),
+  inject.facebookAuth('/facebook-login', 'fb-login')
 ])
 
 /**
@@ -29,22 +41,22 @@ const authInjections = questionInjections.concat([
 /*
   Docs!
 */
-web.route('/', () => web.redirect('/docs/routing'))
-web.htmlRoute('/docs/:doc', 'html/docs.html', async input => ({
+route('/', () => redirect('/docs/routing'))
+htmlRoute('/docs/:doc', 'html/docs.html', async input => ({
   doc: input.doc ? `html/docs/${input.doc}.html` : 'html/docs/routing.html'
 }), injections)
 
 /*
   Questions
 */
-const categories = db.table('categories')
-const questions = db.table('questions')
-const answers = db.table('answers')
-const answersExtended = db.table('answers'
+const categories = mysql.table('categories')
+const questions = mysql.table('questions')
+const answers = mysql.table('answers')
+const answersExtended = mysql.table('answers'
   + ' left join users on users.id = answers.user_id'
   + ' left join questions on questions.id = answers.question_id'
 )
-const questionExtended = db.table('questions'
+const questionExtended = mysql.table('questions'
   + ' left join categories on categories.id = questions.category_id'
   + ' left join users on users.id = questions.user_id'
 )
@@ -65,41 +77,41 @@ const setOwnAnswers = userId => (answer) => {
   return answerWithIsOwnProp
 }
 
-web.htmlRoute('/questions', 'html/questions.html', async (input, session) => ({
+htmlRoute('/questions', 'html/questions.html', async (input, session) => ({
   questions: (await questionExtended.selectFields(questionExtendedFields, null, 'id desc')).map(parseInputText.outputFormat('question')),
   user_id: session.get('user_id'),
   user_name: session.get('user_name'),
-  twitter_url: await web.twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
+  twitter_url: await twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
   categories: await categories.select()
 }), authInjections)
 
-web.htmlRoute('/questions/categories/:category/:id', 'html/questions.html', async (input, session) => ({
+htmlRoute('/questions/categories/:category/:id', 'html/questions.html', async (input, session) => ({
   questions: (await questionExtended.selectFields(questionExtendedFields, { category_id: input.id }, 'id desc')).map(parseInputText.outputFormat('question')),
   user_id: session.get('user_id'),
   user_name: session.get('user_name'),
-  twitter_url: await web.twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
+  twitter_url: await twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
   categories: await categories.select()
 }), authInjections)
 
-web.postRoute('/answers/create', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+postRoute('/answers/create', async (input, session) => {
+  if (!hasAuth(session)) return back()
 
   answers.create({
     question_id: input.question_id,
     answer: input.answer,
-    created: db.now(),
+    created: mysql.now(),
     user_id: session.get('user_id', -1),
     isSolution: false
   })
 
-  return web.back()
+  return back()
 })
 
-web.postRoute('/answers/update', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+postRoute('/answers/update', async (input, session) => {
+  if (!hasAuth(session)) return back()
 
   await answers.update({ answer: input.answer }, { id: input.answer_id })
-  return web.redirect(`/questions/answers/-/-/${input.question_id}`)
+  return redirect(`/questions/answers/-/-/${input.question_id}`)
 })
 
 
@@ -110,7 +122,7 @@ const answersData = async (input, session) => {
   return {
     user_id: session.get('user_id'),
     user_name: session.get('user_name'),
-    twitter_url: await web.twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
+    twitter_url: await twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
     categories: await categories.select(),
     answers: (await answersExtended.selectFields(answerExtendedFields, { question_id: input.id }))
       .map(parseInputText.outputFormat('answer'))
@@ -120,21 +132,21 @@ const answersData = async (input, session) => {
   }
 }
 
-web.htmlRoute(
+htmlRoute(
   '/questions/answers/:name/:title/:id',
   'html/anwers.html',
   answersData,
   questionInjections
 )
 
-web.htmlRoute(
+htmlRoute(
   '/questions/edit-answer/:name/:title/:id/:answer_id',
   'html/anwers.html',
   async (input, session) => {
     const answer = await answers.find({ id: input.answer_id })
     if (answer.user_id !== session.get('user_id')) {
       session.setFlash('warning', 'You need to sign in.')
-      return web.back()
+      return back()
     }
     return Object.assign(
       await answersData(input, session),
@@ -146,17 +158,17 @@ web.htmlRoute(
   questionInjections
 )
 
-web.htmlRoute('/questions/edit/:id', 'html/questions/edit.html', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+htmlRoute('/questions/edit/:id', 'html/questions/edit.html', async (input, session) => {
+  if (!hasAuth(session)) return back()
   const question = await questions.find({ id: input.id })
 
   if (question.user_id !== session.get('user_id')) {
-    return web.back()
+    return back()
   }
   return {
     user_id: session.get('user_id'),
     user_name: session.get('user_name'),
-    twitter_url: await web.twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
+    twitter_url: await twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
     categories: (await categories.select()).map((c) => {
       c.isSelected = question.category_id === c.id ? 'selected' : ''
       return c
@@ -165,13 +177,13 @@ web.htmlRoute('/questions/edit/:id', 'html/questions/edit.html', async (input, s
   }
 }, questionInjections)
 
-web.htmlRoute('/questions/new', 'html/questions/new.html', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+htmlRoute('/questions/new', 'html/questions/new.html', async (input, session) => {
+  if (!hasAuth(session)) return back()
 
   return {
     user_id: session.get('user_id'),
     user_name: session.get('user_name'),
-    twitter_url: await web.twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
+    twitter_url: await twitterRequestUrl('https://js-web-framework.com/twitter-login', session),
     categories: await categories.select()
   }
 }, questionInjections)
@@ -188,9 +200,9 @@ const validateQuestionInput = (input, session) => {
   return true
 }
 
-web.postRoute('/questions/update', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
-  if (!validateQuestionInput(input, session)) return web.back()
+postRoute('/questions/update', async (input, session) => {
+  if (!hasAuth(session)) return back()
+  if (!validateQuestionInput(input, session)) return back()
 
   await questions.update({
     category_id: input.category,
@@ -198,52 +210,52 @@ web.postRoute('/questions/update', async (input, session) => {
     question: input.question
   }, { id: input.id })
 
-  return web.redirect(`/questions/answers/-/-/${input.id}`)
+  return redirect(`/questions/answers/-/-/${input.id}`)
 })
 
-web.route('/questions/delete/:id', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+route('/questions/delete/:id', async (input, session) => {
+  if (!hasAuth(session)) return back()
   const question = await questions.find({ id: input.id })
-  if (question.user_id !== session.get('user_id')) return web.back()
+  if (question.user_id !== session.get('user_id')) return back()
   await questions.delete({ id: question.id })
-  return web.redirect('/questions')
+  return redirect('/questions')
 })
 
-web.route('/answers/delete/:id', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+route('/answers/delete/:id', async (input, session) => {
+  if (!hasAuth(session)) return back()
   const answer = await answers.find({ id: input.id })
-  if (answer.user_id !== session.get('user_id')) return web.back()
+  if (answer.user_id !== session.get('user_id')) return back()
   await answers.delete({ id: answer.id })
-  return web.redirect(`/questions/answers/-/-/${answer.question_id}`)
+  return redirect(`/questions/answers/-/-/${answer.question_id}`)
 })
 
-web.postRoute('/questions/create', async (input, session) => {
-  if (!hasAuth(session)) return web.back()
+postRoute('/questions/create', async (input, session) => {
+  if (!hasAuth(session)) return back()
   if (validateQuestionInput(input, session)) {
     await questions.create({
       category_id: input.category,
       title: input.title,
       question: input.question,
       user_id: session.get('user_id'),
-      created: db.now()
+      created: mysql.now()
     })
-    return web.redirect('/questions')
+    return redirect('/questions')
   }
-  return web.back()
+  return back()
 })
 
 /*
   Tutorials
 */
-web.route('/tutorials', () => web.redirect('/tutorials/hello-world'))
-web.htmlRoute('/tutorials/:tutorial', 'html/tutorials.html', async input => ({
+route('/tutorials', () => redirect('/tutorials/hello-world'))
+htmlRoute('/tutorials/:tutorial', 'html/tutorials.html', async input => ({
   tut: input.tutorial ? `html/tuts/${input.tutorial}.html` : 'html/tuts/hello-world.html'
 }), questionInjections)
 
 /*
   AUTH
 */
-web.postRoute('/google-login', async (input, session) => {
+postRoute('/google-login', async (input, session) => {
   let googleAccess = await auth.googleAccessExists(input)
   if (!googleAccess) {
     googleAccess = await auth.createGoogleAccess(input)
@@ -251,13 +263,13 @@ web.postRoute('/google-login', async (input, session) => {
   const user = await auth.getUserByAccess(googleAccess)
   session.set('user_id', user.id)
   session.set('user_name', user.name)
-  return web.back()
+  return back()
 })
 
-web.postRoute('/facebook-login', async (input, session) => {
+postRoute('/facebook-login', async (input, session) => {
   let facebookAccess = await auth.facebookAccessExists(input)
   if (!facebookAccess) {
-    const extendedData = await web.social.getFacebookFields(input.accessToken)
+    const extendedData = await social.getFacebookFields(input.accessToken)
     facebookAccess = await auth.createFacebookAccess(
       Object.assign(input, extendedData)
     )
@@ -265,10 +277,10 @@ web.postRoute('/facebook-login', async (input, session) => {
   const user = await auth.getUserByAccess(facebookAccess)
   session.set('user_id', user.id)
   session.set('user_name', user.name)
-  return web.back()
+  return back()
 })
 
-web.twitterRoute('/twitter-login', async (input, session) => {
+twitterRoute('/twitter-login', async (input, session) => {
   let twitterAccess = await auth.twitterAccessExists(input)
   if (!twitterAccess) {
     twitterAccess = await auth.createTwitterAccess(input)
@@ -276,10 +288,10 @@ web.twitterRoute('/twitter-login', async (input, session) => {
   const user = await auth.getUserByAccess(twitterAccess)
   session.set('user_id', user.id)
   session.set('user_name', user.name)
-  return web.redirect('/questions')
+  return redirect('/questions')
 })
 
-//web.notFound('html/docs.html', _ => web.redirect('/'))
-web.notFound(null, _ => web.redirect('/'))
+//notFound('html/docs.html', _ => redirect('/'))
+notFound(null, _ => redirect('/'))
 
-web.start()
+start()
